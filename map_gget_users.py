cc2d76b7f453fd103bbed5353cb3c3b3bf43b92e
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-12-05 13:24:05 -0800

    Update map_gget_users.py

--- map_gget_users.py
+++ map_gget_users.py
@@ -113,7 +113,8 @@ def plot_gget_user(property_id):
     cmap = "OrRd"  # Alternative color maps: OrRd YlGn Greens YlOrRd
     col = "user_count"
     world[col] = world[col].astype(int)
-    vmax = world[col].max()
+    # vmax = world[col].max()
+    vmax = 1000
 
     # Remove the axes
     ax.axis("off")
@@ -149,7 +150,7 @@ def plot_gget_user(property_id):
 
     # Add colormap legend
     cbaxes = fig.add_axes([0.15, 0.25, 0.01, 0.4])
-    cbar = fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap, vmax=1000), cax=cbaxes)
+    cbar = fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), cax=cbaxes)
     cbar.set_label(label="Number of active users", size=fontsize, labelpad=-65, c="grey")
     cbar.ax.tick_params(which="both", labelsize=fontsize - 2, color="grey", labelcolor="grey")
 
